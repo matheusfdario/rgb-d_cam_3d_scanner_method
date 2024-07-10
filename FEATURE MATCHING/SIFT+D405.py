904367dca396aea445914c1585dea49be29e4345
matheusfdario <matheusfdario@gmail.com>
2024-07-10 17:48:28 -0300

    test mayavi plots

--- FEATURE MATCHING/SIFT+D405.py
+++ FEATURE MATCHING/SIFT+D405.py
@@ -37,6 +37,34 @@ rec_corners_end = np.array([[129.8,114.8],[711.7,50.7],[138.6,179.0],[716.1,128.
 
 # func
 
+# ==============================================================================
+#                                                                     VIZ_MAYAVI
+# ==============================================================================
+# def viz_mayavi(points, vals="distance"):
+#     x = points[:, 0]  # x position of point
+#     y = points[:, 1]  # y position of point
+#     z = points[:, 2]  # z position of point
+#     # r = lidar[:, 3]  # reflectance value of point
+#     d = np.sqrt(x ** 2 + y ** 2)  # Map Distance from sensor
+#
+#     # Plot using mayavi -Much faster and smoother than matplotlib
+#     import mayavi.mlab
+#
+#     if vals == "height":
+#         col = z
+#     else:
+#         col = d
+#
+#     fig = mayavi.mlab.figure(bgcolor=(0, 0, 0), size=(640, 360))
+#     mayavi.mlab.points3d(x, y, z,
+#                          col,          # Values used for Color
+#                          mode="point",
+#                          colormap='spectral', # 'bone', 'copper', 'gnuplot'
+#                          # color=(0, 1, 0),   # Used a fixed (r,g,b) instead
+#                          figure=fig,
+#                          )
+#     mayavi.mlab.show()
+
 def reject_outliers(data, m = 125.):
     d = np.abs(data - np.median(data))
     mdev = np.median(d)
